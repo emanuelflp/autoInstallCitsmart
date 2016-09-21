24e58d0c5268c81659dec3a52a1f998a4b892c05
Emanuel Felipe Batista de Oliveira <emanuelflp@gmail.com>
2016-09-21 15:17:50 -0300

    Finalizado primeira Release para Teste

--- autoInstallCitsmart.sh
+++ autoInstallCitsmart.sh
@@ -2,8 +2,7 @@
 
 # Script para Instalação automatizada do CitSmart ITSM no Centos 7 ou Centos 6
 
-# Declaração das variaveis
-
+# Declaração das variaveis Gerais
 TMPDIR="/tmp/citsmart";
 INSTALLDIR="/opt/citsmart";
 
@@ -20,7 +19,7 @@ CTSMRTSQLDB="citsmartdb";
 CTSMRTSQLPASSWD=`openssl rand -hex 10`;
 
 fail() { log "\nERRO: $*\n" ; exit 1 ; }
-
+# Verifica Se é Centos nas versões 6 ou 7.
 verificaDistro() {
 
 	if [[ -f /etc/centos-release ]]; then
@@ -50,14 +49,14 @@ verificaDistro() {
 # Primeiro faça o download e instalação dos  Softwares Requisitos.
 prereq() {
 
-	mkdir $TMPDIR &> /dev/null;
+	if [[ `getenforce` = "Enforcing" ]]; then
 
-	# Download do JBOSS
-	wget -P $TMPDIR $URLDOWNLOADJBOSS &> /dev/null || fail "Erro ao fazer o Download do JBoss"
+			setenforce 0
 
-	# Download do Driver JDBC do PostgreSQL
-	wget -P $TMPDIR $URLDOWNLOADPSQLDRIVER &> /dev/null fail "Erro ao fazer o Download do Driver do Banco de dados"
-	
+	fi
+
+	sed -i '/^#/!s/disabled/enforcing/g' /etc/selinux/config
+
 	# Instalação do Repositório do PostgreSQL
 
 	if [[ $VERSAOSO -eq 7 ]]; then
@@ -65,15 +64,21 @@ prereq() {
 		rpm --quiet -ih https://download.postgresql.org/pub/repos/yum/9.3/redhat/rhel-7-x86_64/pgdg-centos93-9.3-2.noarch.rpm &> /dev/null;
 
 	elif [[ $VERSAOSO -eq 6 ]]; then
+
 		rpm --quiet -ih https://download.postgresql.org/pub/repos/yum/9.3/redhat/rhel-6-x86_64/pgdg-centos93-9.3-2.noarch.rpm &> /dev/null;
+
 	fi
-	
-	
 
 	# Instalação dos Requisitos Iniciais do Sistema
 	yum -q -y update &> /dev/null;
 
 	yum -q -y install wget unzip postgresql93-server postgresql93-contrib &> /dev/null || fail "Erro ao fazer a instalação dos pacotes necessários"
+
+	# Download do JBOSS
+	wget -P $TMPDIR $URLDOWNLOADJBOSS &> /dev/null || fail "Erro ao fazer o Download do JBoss"
+
+	# Download do Driver JDBC do PostgreSQL
+	wget -P $TMPDIR $URLDOWNLOADPSQLDRIVER &> /dev/null fail "Erro ao fazer o Download do Driver do Banco de dados"
 
 	# Download do JRE 7.0.40
 	wget -P $TMPDIR/ --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/7u40-b43/jre-7u40-linux-x64.rpm" &> /dev/null || fail "Erro ao fazer o download do JDK do JAVA."
@@ -87,27 +92,21 @@ prereq() {
 
 }
 
-# Iniciando configuração do Banco de dados Postgresql
+# Iniciando configuração do Banco de dados Postgresql.
 dbconfig() {
-
-	if [[ $VERSAOSO -eq 7 ]]; then
-		
-		systemctl enable postgresql-9.3 &> /dev/null;
-
-	elif [[ $VERSAOSO -eq 6 ]]; then
-		chkconfig enable postgresql-9.3
-	fi
 
 	su - postgres -c "/usr/pgsql-9.3/bin/initdb -D $PGSQLDIR/" &> /dev/null;
 
 	echo "host    all             all             127.0.0.1/32               md5" >> $PGSQLDIR/pg_hba.conf &> /dev/null;
 
 	if [[ $VERSAOSO -eq 7 ]]; then
-		
+
+		systemctl enable postgresql-9.3 &> /dev/null;		
 		systemctl start postgresql-9.3 &> /dev/null;
 
 	elif [[ $VERSAOSO -eq 6 ]]; then
 
+		chkconfig enable postgresql-9.3
 		service postgresql-9.3 start
 
 	fi
@@ -118,7 +117,7 @@ dbconfig() {
 
 }
 
-# Configuração do JBOSS para o Citsmart
+# Configuração do JBOSS para o Citsmart.
 jbossconfig() {
 
 	unzip $TMPDIR/jboss-as-7.1.1.Final.zip -d /opt/;
@@ -134,12 +133,53 @@ jbossconfig() {
 	mkdir -p $JBOSSDIR/modules/org/postgresql/main;
 
 	cp $TMPDIR/postgresql-9.3-1103.jdbc41.jar $JBOSSDIR/modules/org/postgresql/main/;
+	cp $TMPDIR/autoinstallCitsmart/files/module.xml $JBOSSDIR/modules/org/postgresql/main/;
+	cp -R $TMPDIR/autoinstallCitsmart/files/standalone.xml $JBOSSDIR/standalone/configuration/;
+	cp -R $TMPDIR/autoinstallCitsmart/files/standalone.conf $JBOSSDIR/bin/;
+	cp -R $TMPDIR/autoinstallCitsmart/files/citsmart.cfg $JBOSSDIR/standalone/configuration/;
 
 }
+
+citsmartconfig(){
+
+	mkdir -p $INSTALLDIR/Lucene/AnexosBaseConhecimento
+	mkdir -p $INSTALLDIR/Lucene/BaseConhecimento
+	mkdir -p $INSTALLDIR/Lucene/PalavraGemea
+	mkdir -p $INSTALLDIR/gedCitsmart
+	mkdir -p $INSTALLDIR/Upload
+
+	chown -fR jboss.jboss $INSTALLDIR/
+
+	sed -i -e 's/CITSMARTSQLUSER/$CTSMRTSQLUSER/g' $JBOSSDIR/standalone/configuration/standalone.xml;
+	sed -i -e 's/CITSMARTSQLPASSWORD/$CTSMRTSQLPASSWD/g' $JBOSSDIR/standalone/configuration/standalone.xml;
+	sed -i -e 's/CITSMARTSQLDB/$CTSMRTSQLPASSWD/g' $JBOSSDIR/standalone/configuration/standalone.xml;
+
+}
+
 
 installpackage(){
 
+	verificaDistro;
 	prereq;
 	dbconfig;
-	jboss;
+	jbossconfig;
+	citsmartconfig;
+	if [[ $? -eq 0 ]]; then
+		clear
+		echo "Instalação Realizada com Sucesso"
+		echo "Dados de acesso ao JBOSS e ao Banco estão logo abaixo"
+		echo "Guarde esses dados caso precise acessa-los"
+		echo
+		echo
+		echo "Para acessar a interface de gerenciamento do JBoss acesse http://IP-SERVIDOR:9990/"
+		echo "Usuario JBoss: admin"
+		echo "Senha JBoss: $JBOSSADMINPASSWORD"
+		echo
+		echo "Usuario Postgresql CitSmart: $CITSMARTSQLUSER"
+		echo "Senha Postgresql CitSmart: $CITSMARTSQLPASSWORD"
+		echo "Nome do Banco CitSmart: $CITSMARTSQLDB"
+		echo
+	fi
 }
+
+installpackage;
--- installer.sh
+++ installer.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+WHOAMI=`whoami`
+
+if [[ ! $WHOAMI = "root" ]]; then
+	echo "Este script deve ser executado pelo root";
+	exit 1;
+fi
+
 yum -q -y install git &> /dev/null
 
 mkdir /tmp/citsmart
